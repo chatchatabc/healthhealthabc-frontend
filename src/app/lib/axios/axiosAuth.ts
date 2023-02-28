29b8a2782a2ffdfb5889ea2e6e11b022b657403c
BoJoNVi <bonjomontes@gmail.com>
2023-02-28 14:22:57 +0800

    feat: implemented login api call to the server

--- src/app/lib/axios/axiosAuth.ts
+++ src/app/lib/axios/axiosAuth.ts
@@ -3,3 +3,7 @@ import { axiosPostJson } from "./axiosActions";
 export const axiosAuthRegister = (body: object) => {
   return axiosPostJson("/auth/register", body);
 };
+
+export const axiosAuthLogin = (body: object) => {
+  return axiosPostJson("/auth/login", body);
+};
---
+++ src/app/login/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React from "react";
+import { AxiosError } from "axios";
+import { Button, Form, Input, Alert } from "antd";
+import { axiosAuthLogin } from "../lib/axios/axiosAuth";
+
+function LoginPage() {
+  const [loading, setLoading] = React.useState(false);
+  const [errorText, setErrorText] = React.useState<null | React.ReactNode>(
+    null
+  );
+
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    try {
+      const res = await axiosAuthLogin(values);
+      console.log("Success:", values, res);
+    } catch (err) {
+      const error = err as AxiosError;
+      if (error.code === "ERR_BAD_REQUEST")
+        setErrorText("Incorrect credentials");
+      else {
+        setErrorText("Connection timed out");
+      }
+      console.log("Error:", error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <main className="flex-1 grid place-items-center">
+      <div className="mx-auto">
+        <h1 className="text-2xl text-center">Login Page</h1>
+        {errorText && (
+          <Alert
+            className="mt-8 text-uppercase"
+            message={errorText}
+            type="error"
+          />
+        )}
+
+        <Form
+          className="mt-8"
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          style={{ maxWidth: "400px" }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 19 }}>
+            <Button loading={loading} type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </main>
+  );
+}
+
+export default LoginPage;
