750366c39a5025abf1c10b6532e477af3d4c2c36
BoJoNVi <bonjomontes@gmail.com>
2023-03-10 15:31:53 +0800

    feat: implemented basic layout of doctors page

--- admin/src/app/doctors/page.tsx
+++ admin/src/app/doctors/page.tsx
@@ -1,7 +1,160 @@
+"use client";
+
 import React from "react";
+import { Space, Table, Tag, Input, Select } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import type { ColumnsType } from "antd/es/table";
 
 function page() {
-  return <div>Doctors</div>;
+  type TablePaginationPosition = "topRight" | "bottomRight" | "bottomCenter";
+
+  const { Search } = Input;
+
+  interface DataType {
+    key: string;
+    name: string;
+    age: number;
+    address: string;
+    tags: string[];
+  }
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+      render: (_, { tags }) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+
+  const data: DataType[] = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sydney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
+
+  return (
+    <div className="px-8">
+      <header className="py-8">
+        <h1 className="text-4xl font-medium">Doctors</h1>
+      </header>
+      <section className="flex items-center gap-2">
+        <Search
+          placeholder="Search doctors..."
+          allowClear
+          enterButton="Search"
+          style={{ width: 350 }}
+          prefix={<SearchOutlined />}
+        />
+
+        <Select
+          showSearch
+          style={{ width: 200 }}
+          placeholder="Specialization"
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            (option?.label ?? "").includes(input)
+          }
+          filterSort={(optionA, optionB) =>
+            (optionA?.label ?? "")
+              .toLowerCase()
+              .localeCompare((optionB?.label ?? "").toLowerCase())
+          }
+          options={[
+            {
+              value: "1",
+              label: "Not Identified",
+            },
+            {
+              value: "2",
+              label: "Closed",
+            },
+            {
+              value: "3",
+              label: "Communicated",
+            },
+            {
+              value: "4",
+              label: "Identified",
+            },
+            {
+              value: "5",
+              label: "Resolved",
+            },
+            {
+              value: "6",
+              label: "Cancelled",
+            },
+          ]}
+        />
+      </section>
+      <section className="mt-8">
+        <Table
+          pagination={{ position: ["topRight" as TablePaginationPosition] }}
+          columns={columns}
+          dataSource={data}
+        />
+      </section>
+    </div>
+  );
 }
 
 export default page;
